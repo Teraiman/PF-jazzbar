989ca0a605103368a434704f8b7a51bbf61cab61
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-08-12 12:41:42 +0000

    [finish]コントローラー[start]top,header

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,40 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
 
+  ########## 会員 ##########
+scope module: :customer do
+  # 会員/ホーム
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: 'about'
+    get 'homes/access', to: 'homes#access', as: 'access'
+
+  #会員/会員
+    resources :customers, only: [:show, :edit]
+    get 'customers/quit', to: 'customers#quit'
+    get 'customers/quitcheck', to: 'customers#quitcheck'
+
+  #会員/予約
+    resources :reserves, only: [:new, :index, :show]
+    get 'reserves/finish', to: 'reserves#finish'
+
+  #会員/スケジュール
+    resources :schedules, only: [:index, :show]
+    get 'schedules/index_list'
+
+  #会員/インフォメーション
+    resources :informations, only: [:index, :show]
+
+  #会員/アーティスト
+
+  #会員/ハウスルール
+    get 'house_rules/index'
+
+  #notification項目の有無？
+end
+  ########## 会員 ##########
 
   ########## 管理者 ##########
   namespace :admin do
-
-  #管理者/アーティスト
-    resources :artists, only: [:index, :edit]
-
-  #管理者/予約
-    resources :reserves, only: [:new, :index, :show, :edit]
-
-  #管理者/スケジュール
-    resources :schedules, only: [:new, :index, :show, :edit]
-    get 'schedules/index_list', to: 'schedules#index_list'
-
-  #管理者/ハウスルール
-    get 'house_rules/index'
-
-  #管理者/インフォメーション
-    resources :informations, only: [:new, :index, :show, :edit]
 
   #管理者/会員
     resources :customers, only: [:index, :show, :edit]
@@ -33,38 +48,22 @@ Rails.application.routes.draw do
     get 'homes/top', to: 'homes#top'
     get 'homes/about', to: 'homes#about', as: 'about'
     get 'homes/access', to: 'homes#access', as: 'access'
+
+  #管理者/スケジュール
+    resources :schedules, only: [:new, :index, :show, :edit]
+    get 'schedules/index_list', to: 'schedules#index_list'
+
+  #管理者/アーティスト
+    resources :artists, only: [:index, :edit]
+
+  #管理者/予約
+    resources :reserves, only: [:new, :index, :show, :edit]
+
+  #管理者/インフォメーション
+    resources :informations, only: [:new, :index, :show, :edit]
+
+  #管理者/ハウスルール
+
   end
   ########## 管理者 ##########
-
-  ########## 会員 ##########
-  namespace :customer do
-  #会員/アーティスト
-    get 'artists/index'
-
-  #会員/予約
-    resources :reserves, only: [:new, :index, :show]
-    get 'reserves/finish', to: 'reserves#finish'
-
-  #会員/スケジュール
-    resources :schedules, only: [:index, :show]
-    get 'schedules/index_list'
-
-  #会員/ハウスルール
-    get 'house_rules/index'
-
-  #会員/インフォメーション
-    resources :informations, only: [:index, :show]
-
-  #会員/会員
-    resources :customers, only: [:show, :edit]
-    get 'customers/quit', to: 'customers#quit'
-    get 'customers/quitcheck', to: 'customers#quitcheck'
-
-  #会員/ホーム
-    root to: 'homes#top'
-    get 'homes/about', to: 'homes#about', as: 'about'
-    get 'homes/access', to: 'homes#access', as: 'access'
-  end
-  ########## 会員 ##########
-  #notification項目の有無？
 end
