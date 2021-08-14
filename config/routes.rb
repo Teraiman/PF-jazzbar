059c34bfd94be04eb7241034ddb78d4245d2cd2f
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-08-14 01:16:38 +0000

    [start]sign_upページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :admins
-  devise_for :customers
+  
+  # devise_for :admins
+  devise_for :admins, module: "admins"
+  # devise_for :customers
+  devise_for :customers, module: "customers"
 
   ########## 会員 ##########
 scope module: :customer do
