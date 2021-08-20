4304695ab1e414a9df6f61e265c6cfe4de277e0d
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-08-20 13:20:43 +0000

    [start]admin/customer[update]index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,19 +1,41 @@
 class Admin::CustomersController < ApplicationController
-  def quit
-  end
+  before_action :authenticate_admin!
 
-  def quitcheck
+  def index
+    #@customers = Customer.all
+    @customers = Customer.page(params[:page]).reverse_order
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
-
-  def index
-  end
-
+  
   def edit_memo
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+
+private
+
+   def customer_params
+     	params.require(:customer).permit(
+     	  :name, 
+     	  :name_kana, 
+     	  :email, 
+     	  :postal_cord, 
+     	  :address, 
+     	  :telephone_number, 
+     	  :memo,
+     	  :is_deleted
+     	  )
+   end
 end
