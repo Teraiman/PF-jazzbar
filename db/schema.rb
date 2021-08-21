6b6997197e884a2b627808aeaa329607f5dc414d
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-08-21 08:06:55 +0000

    [finish]image_idカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_095923) do
+ActiveRecord::Schema.define(version: 2021_08_21_080500) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_095923) do
     t.text "other"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
