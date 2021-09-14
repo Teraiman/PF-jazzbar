f95f3236d6483addae9c4cb60e7be36208f27501
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-09-14 14:12:18 +0000

    [start]RSpec

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,7 @@
 class Customer::HomesController < ApplicationController
   def top
     @informations = Information.all
-    @schedule = Schedule.where(date: Date.today).order(time: "ASC").first
+    @schedule = Schedule.where(date: Date.today.in_time_zone('Tokyo')).order(time: "ASC").first
   end
 
   def about
