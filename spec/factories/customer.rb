1b6526bf43461ac17f3bb8ae0ef2837109781374
Teraiman <toshi.guitar.no1.9995@gmaiil.com>
2021-10-02 06:00:10 +0000

    [save]rspec model customer

---
+++ spec/factories/customer.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :customer do #factory :testcustomer, class: Customer do のようにクラスを明示すればモデル名以外のデータも作れます
+    name { "Spec太郎" }
+    name_kana { "スペックタロウ" }
+    email { "spec@example.com" }
+    postal_cord { "1234567" }
+    address { "住所県住所市" }
+    telephone_number { "09012345678" }
+    memo { "ボトルキープ" }
+    # is_deleted { Faker:: }
+  end
+end
